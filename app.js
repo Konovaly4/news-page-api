741ce8b019cb3080ba205f6bac5e2c9a0ca5badd
Maxim Konovalov <maxkonovalov82@yandex.ru>
2020-05-31 23:23:02 +0300

    one origin

--- app.js
+++ app.js
@@ -28,13 +28,9 @@ mongoose.connect(`mongodb://${SERVERADRESS}:27017/newsdb`, mongoConfig);
 
 // app additional middlewares usage
 app.use((req, res, next) => {
-  console.log(req);
-  const { origin } = req.headers;
-  if (corsOptions.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  }
+  res.header('Access-Control-Allow-Origin', 'https://news-page.gq');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 app.use(limiter);
